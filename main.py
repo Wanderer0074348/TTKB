4f6e9419003d0a13c869a5b0d1dbc7d0e87667e3
Tanay <tanaymatta27@gmail.com>
2025-06-11 14:31:56 +0400

    yadaydayda

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
+import base64
 from openai import OpenAI
 
 client = OpenAI()
 
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+image_path =  # here the dataset will be fed in(ofc implementation will change lol)
+
+base64_image = encode_image(image_path)
+
+
 response = client.responses.create(
-     model = "gpt-4.1",
-     input = "This is a sample text input"
+    model="gpt-4.1",
+    input=[
+        {
+            "role": "user",
+            "content": [
+                { "type": "input_text", "text": "what's in this image?" },
+                {
+                    "type": "input_image",
+                    "image_url": f"data:image/jpeg;base64,{base64_image}", # might need to change shit here also
+                },
+            ],
+        }
+    ],
 )
 
-
-if __name__ == "__main__":
-     print("Hello World")
+print(response.output_text)
